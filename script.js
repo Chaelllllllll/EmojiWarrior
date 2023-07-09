203fbae36fd4df2d523a014f4a82492ff253ea17
John Michael Manlangit <114377074+Chaelllllllll@users.noreply.github.com>
2023-07-09 16:08:18 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+function insertEmoji() {
+    var sentenceInput = document.getElementById("sentenceInput");
+    var emojiInput = document.getElementById("emojiInput");
+    var result = document.getElementById("result");
+
+    var sentence = sentenceInput.value;
+    var emoji = emojiInput.value;
+
+    if (sentence === "") {
+        result.innerHTML = "Oopps mag-type ka muna girl";
+        return;
+    } else if (emoji === "") {
+        result.innerHTML = "Oopps type mo muna emoji na gusto mo";
+        return;
+    } else if (!isEmoji(emoji)) {
+        result.innerHTML = "Oopps hindi valid ang emoji.";
+        return;
+    }
+
+    var words = sentence.match(/\S+/g);
+    var finalSentence = "";
+
+    for (var i = 0; i < words.length; i++) {
+        finalSentence += words[i] + " ";
+        finalSentence += "<span class='emoji'>" + emoji + "</span> ";
+    }
+
+    result.innerHTML = finalSentence;
+}
+
+function isEmoji(str) {
+    // Regular expression to check if the string contains at least one emoji
+    var emojiRegex = /[\uD800-\uDFFF]./;
+    return emojiRegex.test(str);
+}
